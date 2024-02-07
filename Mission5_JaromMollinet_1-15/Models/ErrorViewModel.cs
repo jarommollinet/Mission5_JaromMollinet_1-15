b070bc08a4c3d2f5c04e5327b48578901b76f1e4
Jarom Mollinet <jarommollinet@gmail.com>
2024-02-07 10:39:42 -0700

    Add project files.

---
+++ Mission5_JaromMollinet_1-15/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mission5_JaromMollinet_1_15.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
